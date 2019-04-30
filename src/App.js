6c57a55c7864d08d721e0762caa2becfd7e6d385
Katherine Santiago <santiago.kat@gmail.com>
2019-04-30 15:51:22 -0400

    add some styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return(
-      <div>Countdown Champ, App Component</div>
+      <div className="App">
+        <h1>Countdown to December 25, 2019</h1>
+        <div className="clock-days">14 Days</div>
+        <div className="clock-hours">20 Hours</div>
+        <div className="clock-minutes">40 Minutes</div>
+        <div className="clock-seconds">17 Minutes</div>
+
+        <div>
+          <input placeholder="new date" />
+          <button>Submit</button>
+        </div>
+      </div>
     )
   }
 }
